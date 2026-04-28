d9f299a402332219ff110367c3e96d68e8b541ad
luanstavares <luanstavares29@gmail.com>
2026-04-28 23:54:33 +0100

    fix(prisma): update UUID generation for User model
    Replaced default UUID generation with dbgenerated function to ensure
    compatibility with PostgreSQL's gen_random_uuid() for better
    performance and reliability.

--- app/(tabs)/user-profile.tsx
+++ app/(tabs)/user-profile.tsx
@@ -1,10 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { useAuth } from "@clerk/expo";
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 const UserProfile = () => {
+    const { signOut } = useAuth();
+
     return (
         <View className="flex-1 items-center justify-center bg-background px-4">
             <Text className="text-foreground">User Profile</Text>
+            <Button variant={"secondary"} onPress={() => signOut()}>
+                <Text>Sign Out</Text>
+            </Button>
+            <Button variant={"default"} onPress={() => signOut()}>
+                <Text>Sign Out</Text>
+            </Button>
         </View>
     );
 };
